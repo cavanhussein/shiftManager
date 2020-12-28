44cac0bfe5b1d03c175cbfd121817937e00886e2
Cavan Hussein <cavanhussein@gmail.com>
2020-12-28 15:30:45 -0600

    Add shift update dto constructor.

--- src/main/java/com/when/i/work/shift/dto/ShiftUpdateDto.java
+++ src/main/java/com/when/i/work/shift/dto/ShiftUpdateDto.java
@@ -13,6 +13,11 @@ public class ShiftUpdateDto {
     @JsonFormat(pattern="yyyy-MM-dd@HH:mm:ss")
     private Date endTime;
 
+    public ShiftUpdateDto(Date startTime, Date endTime) {
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
     public Date getStartTime() {
         return startTime;
     }
