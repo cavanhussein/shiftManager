5ebcd6679ab68de38c53e1a2c0e1a8a2e5671f4e
Cavan Hussein <cavanhussein@gmail.com>
2020-12-28 14:49:39 -0600

    Add find overlapping shift query.

--- src/main/java/com/when/i/work/shift/repository/ShiftRepository.java
+++ src/main/java/com/when/i/work/shift/repository/ShiftRepository.java
@@ -2,8 +2,10 @@ package com.when.i.work.shift.repository;
 
 import com.when.i.work.shift.dto.Shift;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.Date;
 import java.util.List;
 
 public interface ShiftRepository extends MongoRepository<Shift, String> {
@@ -11,5 +13,13 @@ public interface ShiftRepository extends MongoRepository<Shift, String> {
 
     List<Shift> findAllByUserId(@Param("userId") String userId);
 
+    @Query("{ $and: ["
+            + "{ 'userId': ?0 },"
+            + "{$or: ["
+            + "{ $and: [{ 'startTime': { $lt: ?1 } }, { 'endTime': { $gt: ?1 } } ]},"
+            + "{ $and: [{ 'startTime': { $lt: ?2 } }, { 'endTime': { $gt: ?2 } } ]}"
+            + "]}"
+            + "]}")
+    List<Shift> findOverlappingShift(String userId, Date startTime, Date endTime);
 
 }
